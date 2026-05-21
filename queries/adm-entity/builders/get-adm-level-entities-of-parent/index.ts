8fc9f8f25c41fd9c6864395ffdc53bb96f8403f4
Tolojanahary | Tolotra | Tolojah <tolotrarabefaly@gmail.com>
2026-05-21 13:32:43 +0300

    feat: Remove the builders

--- queries/adm-entity/builders/get-adm-level-entities-of-parent/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./get-adm-level-entities-of-parent.postgres.query-builder.ts";
-export * from "./get-adm-level-entities-of-parent.sqlite.query-builder.ts";
-export * from "./get-adm-level-entities-of-parent.mysql.query-builder.ts";
-export * from "./get-adm-level-entities-of-parent.mongo.query-builder.ts";

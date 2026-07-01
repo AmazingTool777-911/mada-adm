faf391322da226569ff6ef4cdb66e8eb6baaecae
Tolojanahary | Tolotra | Tolojah <tolotrarabefaly@gmail.com>
2026-07-01 14:13:19 +0300

    refact(map-viewer/components/AdmExplorerPageCountsPills): updates due to app store and some refinements

--- map-viewer/islands/AdmExplorerPageCountsPills.tsx
+++ map-viewer/islands/AdmExplorerPageCountsPills.tsx
@@ -10,14 +10,35 @@ import { injectClientCacheIndexdDbConnection } from "@/client-cache/client-cache
 import { injectAdmGeojsonClientCache } from "@/client-cache/adm-geojson.client-cache.ts";
 import { injectAdmGeojsonStore } from "@/stores/adm-geojson.store.ts";
 import { injectAppMapStore } from "@/stores/app-map.store.ts";
+import { injectApiStore } from "@/stores/api.store.ts";
+import { injectProvinceApi } from "@/api/province.api.ts";
+import { injectRegionApi } from "@/api/region.api.ts";
+import { injectDistrictApi } from "@/api/district.api.ts";
+import { injectCommuneApi } from "@/api/commune.api.ts";
+import { injectFokontanyApi } from "@/api/fokontany.api.ts";
 
-export default function AdmExplorerPageCountsPill() {
+export default function AdmExplorerPageCountsPills() {
   const indexedDb = injectClientCacheIndexdDbConnection();
   const admGeoJsonClientCache = injectAdmGeojsonClientCache(indexedDb);
 
   const admGeoJsonStore = injectAdmGeojsonStore();
 
-  const appMapStore = injectAppMapStore(admGeoJsonClientCache, admGeoJsonStore);
+  const apiStore = injectApiStore();
+  const provinceApi = injectProvinceApi();
+  const regionApi = injectRegionApi();
+  const districtApi = injectDistrictApi();
+  const communeApi = injectCommuneApi();
+  const fokontanyApi = injectFokontanyApi();
+  const appMapStore = injectAppMapStore(
+    admGeoJsonClientCache,
+    admGeoJsonStore,
+    apiStore,
+    provinceApi,
+    regionApi,
+    districtApi,
+    communeApi,
+    fokontanyApi,
+  );
 
   async function handleClick(admLevelCode: AdmLevelCode) {
     await appMapStore.enableAdmGeoJsonLayer(admLevelCode, { fitBbox: true });
@@ -25,17 +46,22 @@ export default function AdmExplorerPageCountsPill() {
 
   return (
     <ul class="m-0 p-0 list-none flex gap-2 flex-wrap">
-      {ADM_LEVEL_CODES_INDEXED.map((admLevelCode) => (
-        <li key={admLevelCode}>
-          <AdmPill
-            admLevelCode={admLevelCode}
-            text={`${ADM_LEVEL_TITLE_BY_CODE.get(admLevelCode)!}s`}
-            badge={ADM_LEVEL_ENTRIES_COUNT_BY_CODE.get(admLevelCode)!
-              .toLocaleString("en-US")}
-            onClick={handleClick}
-          />
-        </li>
-      ))}
+      {ADM_LEVEL_CODES_INDEXED.map((admLevelCode) => {
+        const pluralTitle = `${ADM_LEVEL_TITLE_BY_CODE.get(admLevelCode)!}s`;
+        const count = ADM_LEVEL_ENTRIES_COUNT_BY_CODE.get(admLevelCode)!
+          .toLocaleString("en-US");
+        return (
+          <li key={admLevelCode}>
+            <AdmPill
+              admLevelCode={admLevelCode}
+              text={pluralTitle}
+              title={`${count} ${pluralTitle}`}
+              badge={count}
+              onClick={handleClick}
+            />
+          </li>
+        );
+      })}
     </ul>
   );
 }

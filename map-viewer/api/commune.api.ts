9055dbfcdc3c9a9321669433aa9e993f378e8f7a
Tolojanahary | Tolotra | Tolojah <tolotrarabefaly@gmail.com>
2026-07-06 00:57:57 +0300

    feat(map-viewer/apis/commune): get districts paginated request

--- map-viewer/api/commune.api.ts
+++ map-viewer/api/commune.api.ts
@@ -1,7 +1,19 @@
 import { parseResponse } from "@hono/hono/client";
 import { Commune, EntityId } from "@scope/types/models";
 import { WithRequiredFields } from "@scope/types/utils";
+import {
+  CursorPaginatedResult,
+  GetManyCommunesPaginationCursor,
+} from "@scope/queries/types";
 import { BaseApi } from "@/api/base.api.ts";
+import { ApiCallPaginationParams, ApiRequestOptions } from "@/types/api.d.ts";
+
+export type GetCommunesPaginatedRequestParams = {
+  search?: string;
+  districtId?: EntityId;
+  regionId?: EntityId;
+  provinceId?: EntityId;
+};
 
 export class CommuneApi extends BaseApi {
   async getWithGeoJsonById(
@@ -12,6 +24,30 @@ export class CommuneApi extends BaseApi {
       query: { include_geojson: "1" },
     }));
     return commune as WithRequiredFields<Commune, "geojson">;
+  }
+
+  getManyPaginated(
+    paginationParams: ApiCallPaginationParams,
+    queryParams: GetCommunesPaginatedRequestParams,
+    options?: ApiRequestOptions,
+  ): Promise<
+    CursorPaginatedResult<GetManyCommunesPaginationCursor, Commune>
+  > {
+    return parseResponse(
+      this.client.api.communes.$get(
+        {
+          query: {
+            limit: `${paginationParams.limit}`,
+            cursor: paginationParams.cursor,
+            search: queryParams.search,
+            district_id: queryParams.districtId?.toString(),
+            region_id: queryParams.regionId?.toString(),
+            province_id: queryParams.provinceId?.toString(),
+          },
+        },
+        { init: { signal: options?.signal } },
+      ),
+    );
   }
 }
 

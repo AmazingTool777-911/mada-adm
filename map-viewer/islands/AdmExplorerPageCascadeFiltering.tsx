8146e74d2d9a06012ce63cf7aff3dfa0e459df9f
Tolojanahary | Tolotra | Tolojah <tolotrarabefaly@gmail.com>
2026-07-06 00:58:51 +0300

    feat(map-viewer/pages/AdmExplorer/Commune): communes search field

--- map-viewer/islands/AdmExplorerPageCascadeFiltering.tsx
+++ map-viewer/islands/AdmExplorerPageCascadeFiltering.tsx
@@ -1,16 +1,18 @@
 import { useSignal } from "@preact/signals";
-import { District, Province, Region } from "@scope/types/models";
+import { Commune, District, Province, Region } from "@scope/types/models";
 import AdmExplorerPageCascadeFilteringProvince from "@/islands/AdmExplorerPageCascadeFilteringProvince.tsx";
 import AdmExplorerPageCascadeFilteringRegion from "@/islands/AdmExplorerPageCascadeFilteringRegion.tsx";
 import AdmExplorerPageCascadeFilteringDistrict from "@/islands/AdmExplorerPageCascadeFilteringDistrict.tsx";
+import AdmExplorerPageCascadeFilteringCommune from "@/islands/AdmExplorerPageCascadeFilteringCommune.tsx";
 
 export default function AdmExplorerPageCascadeFiltering() {
   const selectedProvince = useSignal<Province | null>(null);
   const selectedRegion = useSignal<Region | null>(null);
   const selectedDistrict = useSignal<District | null>(null);
+  const selectedCommune = useSignal<Commune | null>(null);
 
   return (
-    <form class="space-y-3 pb-4">
+    <form class="space-y-3 pb-60">
       <AdmExplorerPageCascadeFilteringProvince
         selectedProvince={selectedProvince.value}
         onSelectedChange={(province) => selectedProvince.value = province}
@@ -25,6 +27,13 @@ export default function AdmExplorerPageCascadeFiltering() {
         selectedRegion={selectedRegion.value}
         selectedDistrict={selectedDistrict.value}
         onSelectedChange={(district) => selectedDistrict.value = district}
+      />
+      <AdmExplorerPageCascadeFilteringCommune
+        selectedProvince={selectedProvince.value}
+        selectedRegion={selectedRegion.value}
+        selectedDistrict={selectedDistrict.value}
+        selectedCommune={selectedCommune.value}
+        onSelectedChange={(commune) => selectedCommune.value = commune}
       />
     </form>
   );
